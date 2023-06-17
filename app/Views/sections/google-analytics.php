663ba8d1bc96033b8502b5dbb063d2aa7918a664
John Skoubourdis <scoumbourdisj@gmail.com>
2023-06-17 15:33:38 +0300

    New Google Analytics

--- app/Views/sections/google-analytics.php
+++ app/Views/sections/google-analytics.php
@@ -1,9 +1,9 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23493740-5"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RCSFB14YRL"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-23493740-5');
+    gtag('config', 'G-RCSFB14YRL');
 </script>
